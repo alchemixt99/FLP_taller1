635b32521c7d527622f3c10e787b163df956fbfe
alchemixt99 <dos.choco@gmail.com>
2015-11-23 09:01:17 -0500

    punto 2

--- Taller1flp.rkt
+++ Taller1flp.rkt
@@ -4,8 +4,7 @@
 ;;
 
 
-
-;;************** Punto 2 **************
+;;************** Punto 1 **************
 ;;<lista-de-elementos> := ()
 ;;list-tails : lista -> lista2
 ;;
@@ -24,3 +23,27 @@
 ;;Pruebas
 ;;(list-tails '(1 2 3 4 5 ))
 ;;(list-tails '(1 a (e 4) 5 v))
+
+;;************** Punto 3 **************
+;;<lista-de-elementos> := ()
+;;list-facts: n -> lista
+;;
+;;   Propósito:
+;;       Este procedimiento recibe como par parámetro un entero n y retorna una lista incremental de factoriales,
+;;       comenzando en 1! hasta n!
+;;
+(define aux-lf
+  (lambda (act ant n)
+    (if (eqv? act (+ n 1))
+        '()
+        (cons (* act ant) (aux-lf (+ act 1)(* act ant) n))))) 
+
+
+(define list-facts
+  (lambda (n)
+    (if (= 0 n)
+        0
+        (aux-lf 1 1 n)))) 
+
+;;Pruebas
+;;(list-facts 5)
