45d7e7153a138450d243ddea14bb6a5113d31d34
alchemixt99 <dos.choco@gmail.com>
2015-11-23 10:20:53 -0500

    pasando a datatype

--- Taller1flp.rkt
+++ Taller1flp.rkt
@@ -12,14 +12,26 @@
 ;;       Este procedimiento recibe una lista y con esta construye una nueva usando 
 ;;       las sublistas de elementos consecutivos en la lista inicial.
 ;;
-(define list-tails
+(define-datatype list list?
+  (empty-list)
+  (non-empty-list (first number?)
+                  (rest list?))
+)
+(define-datatype s-exp s-exp?
+  (symbol-s-exp (sym symbol?))
+  (number-s-exp (num number?))
+  (s-list-s-exp (slst list?))
+)
+
+
+(define list-tails 
   (lambda (ls)
-    (if (null? ls)
-        '()
-        (cons ls (list-tails (cdr ls)))
-        )
-    )
+  (cases list ls
+    (empty-list () #t)
+    (non-empty-list (ls lx) #f) 
   )
+   )
+)
 ;;Pruebas
 ;;(list-tails '(1 2 3 4 5 ))
 ;;(list-tails '(1 a (e 4) 5 v))
@@ -38,7 +50,6 @@
         '()
         (cons (* act ant) (aux-lf (+ act 1)(* act ant) n))))) 
 
-
 (define list-facts
   (lambda (n)
     (if (= 0 n)
@@ -47,3 +58,12 @@
 
 ;;Pruebas
 ;;(list-facts 5)
+
+;;************** Punto 5 **************
+;;<lista-de-elementos> := ()
+;;list-facts: n -> lista
+;;
+;;   Propósito:
+;;       Este procedimiento  determina el número de inversiones de lista. Sea A = (a1, a2 . . . an) una
+;;       lista de n números diferentes, si i < j y ai > aj entonces la pareja (i j) es una inversión de A.
+;;
